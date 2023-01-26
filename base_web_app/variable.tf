387651ce764727e2d6b979ca422d1140d211c6ea
Ryshant Srivastava <rishant.srivastava11@gmail.com>
2023-01-26 07:37:21 +0530

    Adding Variables through the dedicated file

---
+++ base_web_app/variable.tf
@@ -0,0 +1,63 @@
+variable "aws_access_key" {
+  type = string
+  description = "AWS Access Keys"
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+    type = string
+    description = "AWS Secret keys"
+    sensitive = true
+}
+
+variable "aws_region" {
+    type = string
+    description = "AWS Regions to create a region"
+    default = "us-east-1"
+}
+
+variable "enable_dns_hostnames" {
+    type = bool
+    description = "Enable DNS in the VPC"
+    default = true
+}
+
+variable "vpc_cidr_block" {
+    type = string
+    description = "Base CIDR block for the VPC"
+    default = "10.0.0.0/16"
+}
+
+variable "vpc_subnet1_cidr_block" {
+    type = string
+    description = "Subnet 1 CIDR block for the VPC"
+    default = "10.0.0.0/24"
+}
+
+variable "map_public_ip_on_launch" {
+    type = bool
+    description = "Map a public IP Address for the subnet Instance"
+    default = true
+}
+
+variable "instance_type" {
+    type = string
+    description = "The type of instance to be provisioned."
+    default = "t2.micro"
+}
+
+variable "company" {
+    type = string
+    description = "Common_Tag for resources depicting Company"
+    default = "Paymentus"
+}
+
+variable "project" {
+    type = string
+    description = "Common_Tag for resources depicting the project"
+}
+
+variable "billing_code" {
+    type = string
+    description = "Billing Tag for the resources"
+}
