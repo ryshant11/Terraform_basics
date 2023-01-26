79f62e7a7b3e734efa75e31e1cd3a4887807799c
Ryshant Srivastava <rishant.srivastava11@gmail.com>
2023-01-26 07:39:55 +0530

    Making main.tf final changes to accomodate the local, input variables and output configuration

--- base_web_app/main.tf
+++ base_web_app/main.tf
@@ -3,9 +3,9 @@
 ##################################################################################
 
 provider "aws" {
-  access_key = "AKIA5RBBUXPCH6NAQ6RV"
-  secret_key = "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"
-  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
 }
 
 ##################################################################################
@@ -22,25 +22,33 @@ data "aws_ssm_parameter" "ami" {
 
 # NETWORKING #
 resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  tags = local.common_tags
 
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 
+  tags = local.common_tags
+
 }
 
 resource "aws_subnet" "subnet1" {
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = var.vpc_subnet1_cidr_block
   vpc_id                  = aws_vpc.vpc.id
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+
+  tags = local.common_tags
+
 }
 
 # ROUTING #
 resource "aws_route_table" "rtb" {
   vpc_id = aws_vpc.vpc.id
+  tags = local.common_tags
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -58,6 +66,9 @@ resource "aws_route_table_association" "rta-subnet1" {
 resource "aws_security_group" "nginx-sg" {
   name   = "nginx_sg"
   vpc_id = aws_vpc.vpc.id
+
+  tags = local.common_tags
+
 
   # HTTP access from anywhere
   ingress {
@@ -79,7 +90,7 @@ resource "aws_security_group" "nginx-sg" {
 # INSTANCES #
 resource "aws_instance" "nginx1" {
   ami                    = nonsensitive(data.aws_ssm_parameter.ami.value)
-  instance_type          = "t2.micro"
+  instance_type          = var.instance_type
   subnet_id              = aws_subnet.subnet1.id
   vpc_security_group_ids = [aws_security_group.nginx-sg.id]
 
@@ -90,6 +101,8 @@ sudo service nginx start
 sudo rm /usr/share/nginx/html/index.html
 echo '<html><head><title>Taco Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
 EOF
+
+tags = local.common_tags
 
 }
 
