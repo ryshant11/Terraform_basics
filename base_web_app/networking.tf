a6b615303159c2b98c830c0adafa044870c8e21a
Ryshant Srivastava <rishant.srivastava11@gmail.com>
2023-01-30 08:54:42 +0530

    Added Resiliency Infrastructure changes: ALB, Subnets, RTB and EC2

--- base_web_app/networking.tf
+++ base_web_app/networking.tf
@@ -16,6 +16,10 @@ data "aws_ssm_parameter" "ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 ##################################################################################
 # RESOURCES
 ##################################################################################
@@ -37,9 +41,20 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_subnet" "subnet1" {
-  cidr_block              = var.vpc_subnet1_cidr_block
+  cidr_block              = var.vpc_subnets_cidr_block[0]
   vpc_id                  = aws_vpc.vpc.id
   map_public_ip_on_launch = var.map_public_ip_on_launch
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = local.common_tags
+
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block              = var.vpc_subnets_cidr_block[1]
+  vpc_id                  = aws_vpc.vpc.id
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+  availability_zone = data.aws_availability_zones.available.names[1]
 
   tags = local.common_tags
 
@@ -61,10 +76,38 @@ resource "aws_route_table_association" "rta-subnet1" {
   route_table_id = aws_route_table.rtb.id
 }
 
+resource "aws_route_table_association" "rta-subnet2" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rtb.id
+}
 # SECURITY GROUPS #
 # Nginx security group 
 resource "aws_security_group" "nginx-sg" {
   name   = "nginx_sg"
+  vpc_id = aws_vpc.vpc.id
+
+  tags = local.common_tags
+
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name   = "nginx_alb_sg"
   vpc_id = aws_vpc.vpc.id
 
   tags = local.common_tags
@@ -85,24 +128,5 @@ resource "aws_security_group" "nginx-sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-# INSTANCES #
-resource "aws_instance" "nginx1" {
-  ami                    = nonsensitive(data.aws_ssm_parameter.ami.value)
-  instance_type          = var.instance_type
-  subnet_id              = aws_subnet.subnet1.id
-  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
-
-  user_data = <<EOF
-#! /bin/bash
-sudo amazon-linux-extras install -y nginx1
-sudo service nginx start
-sudo rm /usr/share/nginx/html/index.html
-echo '<html><head><title>Taco Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
-EOF
-
-tags = local.common_tags
-
 }
 
